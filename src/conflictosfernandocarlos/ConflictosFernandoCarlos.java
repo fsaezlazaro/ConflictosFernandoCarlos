bf1928c04d2da2933c8ba458469633ff6c4cdc8d
Fernando Saez <saezlazarof@iesmartinrivero.org>
2022-04-27 14:42:45 +0200

    Cambio de linea

--- src/conflictosfernandocarlos/ConflictosFernandoCarlos.java
+++ src/conflictosfernandocarlos/ConflictosFernandoCarlos.java
@@ -15,7 +15,7 @@ public class ConflictosFernandoCarlos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Primer mensaje Fernando");
+        System.out.println("Segundo mensaje Fernando");
     }
     
 }
